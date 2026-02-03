5e56e2654415fd7255044fb3f2a89c48fc8fbbbb
annivel <annivelichko@gmail.com>
2026-02-03 21:12:23 +0000

    leetcode day5

--- programming/leetcode/30DaysChallenge/day1/helloWorld.ts
+++ programming/leetcode/30DaysChallenge/day1/helloWorld.ts
@@ -1,9 +1,9 @@
 function createHelloWorld() {
   // high-order function example
+  // ...args: any[] -> in tscofig.json (compiler setting) strict is set to true. It doesn't allow implicit any[] types
   return function (...args: any[]): string {
     return "Hello World";
   };
-  // ...args: unknown[] -> in tscofig.json (compiler setting) strict is set to true. It doesn't allow implicit any[] types
 }
 
 const f = createHelloWorld();
