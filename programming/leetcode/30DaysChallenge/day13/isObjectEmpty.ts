6f49eebb87552d63dfad202b152a226d1a3164fc
annivel <annivelichko@gmail.com>
2026-02-24 00:15:58 +0000

    leetcode day13

---
+++ programming/leetcode/30DaysChallenge/day13/isObjectEmpty.ts
@@ -0,0 +1,11 @@
+export type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+    if (Array.isArray(obj)){
+        return obj.length == 0;      
+    } else {
+      return Object.keys(obj).length == 0;
+    }
+
+};
